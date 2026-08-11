4493d7776eea3f204602c9d4a194348416a8cacb
JeffersonMagalhaes <jefferson.raimundomagalhaes@gmail.com>
2026-08-11 00:22:24 -0300

    fix: corrigir sintaxe do start.sh

--- start.sh
+++ start.sh
@@ -2,13 +2,12 @@
 # Startup - Railway
 set -e
 
-# Deriva CHAINLIT_AUTH_SECRET do DEEPSEEK_API_KEY (hash deterministico)
 if [ -z "$DEEPSEEK_API_KEY" ]; then
     echo "ERRO: DEEPSEEK_API_KEY nao definida. Configure no Railway."
     exit 1
 fi
 
-export CHAINLIT_AUTH_SECRET=*** "$DEEPSEEK_API_KEY" | sha256sum | cut -d' ' -f1)
+export CHAINLIT_AUTH_SECRET=$(echo -n "$DEEPSEEK_API_KEY" | sha256sum | cut -d' ' -f1)
 export DATA_DIR="${DATA_DIR:-/data}"
 
 echo "Iniciando Chainlit..."
